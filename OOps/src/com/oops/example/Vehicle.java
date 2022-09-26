483fd99bc4c193a37961c33238255528d4c26b19
shivani-kpipartners <shivani.shukla@kpipartners.com>
2022-09-26 14:51:45 +0530

    Commit code

---
+++ OOps/src/com/oops/example/Vehicle.java
@@ -0,0 +1,9 @@
+package com.oops.example;
+
+public abstract class Vehicle {
+	abstract char model_name();
+	abstract char brand();
+	abstract int sitting_capacity();
+	abstract boolean electric();
+	abstract int mileage() ;
+}
