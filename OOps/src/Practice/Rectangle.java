116658ed3f1bdb0c5f37cee9c52c9b69af17fb6d
shivani-kpipartners <shivani.shukla@kpipartners.com>
2022-09-26 17:59:20 +0530

    commit

---
+++ OOps/src/Practice/Rectangle.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class Rectangle extends Area {
+
+	public static void main(String []args) {
+		
+		System.out.println("I am Rectangle");
+		// TODO Auto-generated method stub
+
+	}
+
+}
